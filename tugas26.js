c44e5ebe37d3d499ad87176953e3198b685dcaf5
Andika Andriana <andriana.mia1@gmail.com>
2019-12-09 15:56:50 +0700

    Tugas 27 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas26.js
@@ -0,0 +1,16 @@
+function tugas26() {
+  a = "No ";
+  b = "rumah saya ";
+  c = String(30);
+  d = "A";
+  hasil1 = a + b + c + d;
+  console.log(hasil1);
+
+  e = "Jalan pangeran Tirtayasa No ";
+  f = String(70);
+
+  hasil2 = e + f;
+  console.log(hasil2);
+}
+
+tugas26();
